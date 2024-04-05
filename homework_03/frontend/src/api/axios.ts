6d95b1d4fcd840fdf54f8ffd7debe71cab0a9754
Cristian Roman <romancristianroman@yahoo.com>
2024-04-05 16:09:06 +0300

    (refactor): changed base url from local to gcloud ip

--- homework_03/frontend/src/api/axios.ts
+++ homework_03/frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
+  baseURL: `${"http://34.128.151.8/api/v1"}`,
   headers: {
     'Content-Type': 'application/json',
   },
