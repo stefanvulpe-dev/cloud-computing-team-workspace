d8bbe2d4a7d5bcbc2638e54e91bfdd3be03e0dfb
stefanvulpe-dev <svulpe25@gmail.com>
2024-05-18 16:47:00 +0300

    chore: restructure project

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
-import cors from 'cors';
 import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 import { authRouter, recipeRouter } from './routes';
 import { RedisService } from './services';
